0eb5fbc7b8396b1d34d0a6501876b7f13916377d
yazseyit77 <yazgeldi.seyidov@gmail.com>
2019-09-28 14:47:07 -0700

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,15 @@ require 'active_record'
 
 Bundler.require
 
+DBNAME = "tvshows"
+
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
 
-DB = ActiveRecord::Base.establish_connection({
-  adapter: 'sqlite3',
-  database: 'db/tvshows.db'
-})
+connection_details = YAML::load(File.open('config/database.yml'))
+
+
+DB = ActiveRecord::Base.establish_connection(connection_details)
 
 if ENV["ACTIVE_RECORD_ENV"] == "test"
   ActiveRecord::Migration.verbose = false
